47ad07e9e17af58d6758fdc05726a1aeb1ca33c9
harada_laptop <harada.kei@ist.osaka-u.ac.jp>
2019-08-05 11:52:37 +0900

    add script

---
+++ script/build.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+# file, scale, gridsize, radis, key, secret
+python ../source/build.py "build.py" "large" 2000 1000 "TUCJCEQMKNPCTXBKUQ0CA2DF5JOGAYT0EVP1SXOJIOB2NCVH" "RDLLQBSZ5UH2FOCNTTL3EANZMLR2VMAYPT5RDS3ADJCXTNGJ"
