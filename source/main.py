d6dff68c836a497902ad9f31ebaf682a99d0725a
Kei Harada <harada.kei@ist.osaka-u.ac.jp>
2019-12-11 23:14:51 +0900

    makeCityDatav2

--- source/main.py
+++ source/main.py
@@ -906,8 +906,7 @@ def cityTest19_cityData(CITIEs4):
 
         print("*---TARGET: {}".format(TARGET))
 
-        #for loop in range(1, 4):
-        for loop in range(1, 2):
+        for loop in range(1, 4):
             print("* Shuffle Loop: {}".format(str(loop)))
 
             # dataset path
