7d9c1fc9ffcc9d35fd042b0ea4cd489578e9bbe1
Kei Harada <harada.kei@ist.osaka-u.ac.jp>
2019-12-15 19:45:43 +0900

    expOfBeiJing

--- source/main.py
+++ source/main.py
@@ -996,7 +996,7 @@ if __name__ == "__main__":
     create dataset
     '''
     #makeDataset(CITIEs20, ATTRIBUTE, LSTM_DATA_WIDTH, TIMEPERIOD)
-    CITIEs4 = ["TianJin", "ShenZhen", "GuangZhou"]
+    CITIEs4 = ["BeiJing"]
     cityTest19_cityData(CITIEs4)
     #cityTest19(CITIEs20, CITIEs4)
     #cityTest5(CITIEs4)
